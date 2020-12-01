5b7b2061019146802d33387c0b815b815b73b0dc
Matheus Giordani <matheus.giordani@pampa.compasso>
2020-12-01 15:03:03 -0300

    validations

--- src/automatic-billing/interfaces/automaticBilling.form.ts
+++ src/automatic-billing/interfaces/automaticBilling.form.ts
@@ -1,5 +1,13 @@
-export interface AutomaticBillingForm {
+import { IsInt, IsNotEmpty, Min } from 'class-validator';
+
+export class AutomaticBillingForm {
+  @IsNotEmpty()
   type: string;
+
+  @Min(1)
   price: number;
+
+  @IsInt()
+  @Min(1)
   payDay: number;
 }
--- src/bills/interfaces/bill.form.ts
+++ src/bills/interfaces/bill.form.ts
@@ -1,10 +1,22 @@
+import { IsEnum, IsInt, IsNotEmpty, Min } from 'class-validator';
 import { PaymentMethod } from '../bill';
-
-export interface BillForm {
+export class BillForm {
+  @IsNotEmpty()
   place: string;
+
+  @IsNotEmpty()
   type: string;
+
+  @Min(1)
   price: number;
+
+  @IsEnum(PaymentMethod)
   paymentMethod: PaymentMethod;
+
+  @IsNotEmpty()
   date: string;
-  installments?: number;
+
+  @Min(1)
+  @IsInt()
+  installments: number;
 }
